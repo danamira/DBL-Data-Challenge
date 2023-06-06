25d38cd5c65bea7aa10ddd8d3d7176e851548415
larsenalicia <alicia.hh.larsen@gmail.com>
2023-06-06 14:56:34 +0200

    dataframe in response_time_stats working properly

--- notebooks/response2.py
+++ notebooks/response2.py
@@ -59,65 +59,19 @@ def response_time_id(tweet_id, cursor):
     return response_time
 
 
-def mentions(mention_str: str):
+def mentions_length(mention_str: str):
     """
-    Takes a list of user_id and returns what airline it corresponds to, or if it is not an airline.
-    :param: mention_list: list of user IDs that have been mentioned in a tweet. 
-    :returns: list of correponding users, ervyone but airlines as "not_airline" 
+    Takes a str(list) of user IDs and returns the length of list(int) of user IDs.
+    :param: mention_str2: str(list) of user IDs.
+    :returns: list of mentioned user IDs.
     """
-    user_mentions_list: list = []
     mention_list = re.findall("\d+", mention_str)
 
     # Convert every ID to int
     for i, id in enumerate(mention_list):
         mention_list[i] = int(id)
 
-    # For every mention ID
-    for mention in mention_list:
-        added = False
-        # For every airline in airlineIDs        
-        for key in airlineIDs:
-            # If ID and airline ID match:            
-            if mention == airlineIDs[key]:
-                user_mentions_list.append(key)
-                added = True
-            continue
-        # If no airline was added:
-        if added == False:
-            user_mentions_list.append('no_airline')
-    
-    return user_mentions_list
-
-def identify_airline(mention_list):
-    """
-    Gets a string and identifies wheather this is an airline or not.
-    :param: mention_list: list of usernames.
-    :returns: the airline.
-    """
-    airline = 'no_airline'
-    for mention in mention_list:
-        for key in airlineIDs:
-            if mention == airlineIDs[key]:
-                airline = key
-
-    return airline
-
-def mention_count(mention_str):
-    """
-    Takes a string of a list of mentions and returns the number of mentions.
-    :param: mention_list: list of mentions IDs.
-    :returns: int value of number of mentions.
-    """
-    return len(mentions(mention_str))
-
-
-def airline():
-    """
-    Takes a string of list of airline names and identifies the airline.
-    :param: airline_list: list of airline names.
-    :returns: the airline in the mentions
-    """
-
+    return int(len(mention_list))
 
 
 def length(text):
