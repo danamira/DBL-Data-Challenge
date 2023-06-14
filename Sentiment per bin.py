2d52c9438a9b72646863256fa3a876a5f9c8bd25
ScarMur <120718882+ScarMur@users.noreply.github.com>
2023-06-14 17:07:35 +0200

    FIle for table creation added

---
+++ Sentiment per bin.py
@@ -0,0 +1,17 @@
+def create_sentim_table (cursor) -> None:
+    """This function will create the table to store the sentiment per bin into in your database.
+    required imports: config.env, cursor as database.cursor(), mysql.connector
+    input: database.cursor() as cursor
+    output: nothing."""
+    database = config.env.getConfig('DB_DATABASE')
+
+    cursor.execute(f"""CREATE TABLE `{database}`.`binned_sentiment`(begin_id BIGINT NOT NULL, end_id BIGINT NOT NULL, cID MEDIUMINT NOT NULL, avg_sentiment DECIMAL(7,1),
+                   PRIMARY KEY(begin_id, cID))""")
+    return None
+
+import mysql.connector 
+from database.connect import getConnection
+import config.env  
+db = getConnection()
+cursor=db.cursor()
+create_sentim_table(cursor)
