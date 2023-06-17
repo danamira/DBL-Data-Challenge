a5ad06005dd975067d335c8c8d03591d8d8a28b0
ScarMur <120718882+ScarMur@users.noreply.github.com>
2023-06-17 11:02:52 +0200

    Removed an unnecessary line

--- Sentiment per bin.py
+++ Sentiment per bin.py
@@ -4,6 +4,5 @@ import config.env
 from sentiment_evolution_functions import create_sentim_table, drop_sentim_table, fill_bin_table, insert_bins
 db = getConnection()
 cursor=db.cursor()
-drop_sentim_table(db, cursor)
 create_sentim_table(db,cursor, config)
 fill_bin_table(db, cursor)
